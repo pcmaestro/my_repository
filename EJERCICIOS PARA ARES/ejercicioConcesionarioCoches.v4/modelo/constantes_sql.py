ef84ae8972ad81a238437eec5a3146a97ff81d2c
pcmaestro <pcmaestro@hotmail.com>
2020-04-19 15:50:03 +0200

    elimandas carpetas obsoletas

--- EJERCICIOS PARA ARES/ejercicioConcesionarioCoches.v4/modelo/constantes_sql.py
+++
@@ -1,9 +0,0 @@
-SQL_INSERCION_COCHE = "INSERT INTO tabla_coches(marca, modelo, color, motor, precio, forma_pago) VALUES (%s,%s,%s,%s,%s,%s);"
-
-SQL_LISTADO_COCHES = "SELECT * FROM tabla_coches;"
-
-SQL_OBTENER_COCHE_POR_ID = "SELECT * FROM tabla_coches WHERE id_producto = %s ;"
-
-SQL_BORRAR_COCHE = "DELETE FROM tabla_coches WHERE id_producto = %s ;"
-
-SQL_GUARDAR_CAMBIOS_COCHE = "UPDATE tabla_coches SET marca = %s, modelo = %s, color = %s, motor = %s , precio = %s , forma_pago = %s WHERE id_producto = %s ;"
