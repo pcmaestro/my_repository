9c4961b475905d14b418ac652fa9d03ce630910e
Pedro Castellanos <pcmaestro@hotmail.com>
2020-07-17 00:45:41 +0200

    17-jul

--- JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -1,33 +1,19 @@
 
 package com.mycompany.mavenproject1;
  
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JList;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
-import javax.swing.ListSelectionModel;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 
 
 public class NewClass {
   public static void main(String[] args) {
       
-      String[] array = {"rojo", "verde", "azul"};
-      JComboBox cbx = new JComboBox(array);
-      
-      cbx.setSelectedIndex(1);
-      
-      int indice = cbx.getSelectedIndex();
-      
-      String seleccionado = array[1];
-      
-      System.out.println(seleccionado);
-      
-      
-      
-      
-      
+      String text = " Lamborghini Diaglo ";
+    
+      System.out.println(text.trim());
       
   }
 }
