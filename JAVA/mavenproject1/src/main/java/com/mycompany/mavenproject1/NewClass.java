ec043c1f46d9901cdc73760182efe672ddcfca0b
Pedro Castellanos <pcmaestro@hotmail.com>
2020-07-21 11:28:04 +0200

    21-jul

--- JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -11,9 +11,23 @@ import java.util.regex.Pattern;
 public class NewClass {
   public static void main(String[] args) {
       
-      String text = " Lamborghini Diaglo ";
-    
-      System.out.println(text.trim());
+      boolean validacion;
+      
+      String text = "1234";
+      
+      Pattern patronEmail = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+		
+      Matcher matcher = patronEmail.matcher(text);
+		
+      boolean matchfound = matcher.find();
+		
+    if(matchfound) {
+            validacion = true;
+    }else {
+            validacion = false;
+    }
+
+    System.out.println("validacion = "+ validacion)
       
   }
 }
