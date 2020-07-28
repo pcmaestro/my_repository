47b10c5c505143880f6efac0e9f79263ecf8647e
Pedro Castellanos <pcmaestro@hotmail.com>
2020-07-28 22:32:53 +0200

    28-jul

--- JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.mavenproject1;
  
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,23 +13,7 @@ import java.util.regex.Pattern;
 public class NewClass {
   public static void main(String[] args) {
       
-      boolean validacion;
-      
-      String text = "1234";
-      
-      Pattern patronEmail = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-		
-      Matcher matcher = patronEmail.matcher(text);
-		
-      boolean matchfound = matcher.find();
-		
-    if(matchfound) {
-            validacion = true;
-    }else {
-            validacion = false;
-    }
-
-    System.out.println("validacion = "+ validacion)
-      
+      Map<Integer, String> mapa = {404, "not found"};
   }
+  
 }
